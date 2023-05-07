8b21651767cc5289ac40b1b2a29a5d091605af4c
Juan Ruiz Valencia <juan7732@gmail.com>
2023-05-07 00:18:07 -0500

    serve index

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.get("/start", (req, res) => {
   res.sendStatus(200);
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(3000, () => {
   console.log("Video streaming server is running on http://localhost:3000");
 
